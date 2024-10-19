463078300584d9a91c3c0134980f7a347bb2a074
Janic Beauchemin <jbeauchemin33@gmail.com>
2024-10-19 00:07:11 -0400

    Add populate-deep plugin

--- backend/src/api/page/middlewares/page-populate-middleware.js
+++ backend/src/api/page/middlewares/page-populate-middleware.js
@@ -4,49 +4,49 @@
  * `page-populate-middleware` middleware
  */
 
-const populate = {
-  contentSections: {
-    populate: {
-      picture: {
-        fields: ["url", "alternativeText", "caption", "width", "height"],
-      },
-      buttons: {
-        populate: true,
-      },
-      feature: {
-        populate: {
-          fields: ["title", "description", "showLink", "newTab", "url", "text"],
-          media: {
-            fields: ["url", "alternativeText", "caption", "width", "height"],
-          },
-        },
-      },
-      testimonials: {
-        populate: {
-          picture: {
-            fields: ["url", "alternativeText", "caption", "width", "height"],
-          },
-        },
-      },
-      plans: {
-        populate: ["product_features"],
-      },
-      submitButton: {
-        populate: true,
-      },
-    },
-  },
-  seo: {
-    fields: ["metaTitle", "metaDescription"],
-    populate: { shareImage: true },
-  }
-};
+// const populate = {
+//   contentSections: {
+//     populate: {
+//       picture: {
+//         fields: ["url", "alternativeText", "caption", "width", "height"],
+//       },
+//       buttons: {
+//         populate: true,
+//       },
+//       feature: {
+//         populate: {
+//           fields: ["title", "description", "showLink", "newTab", "url", "text"],
+//           media: {
+//             fields: ["url", "alternativeText", "caption", "width", "height"],
+//           },
+//         },
+//       },
+//       testimonials: {
+//         populate: {
+//           picture: {
+//             fields: ["url", "alternativeText", "caption", "width", "height"],
+//           },
+//         },
+//       },
+//       plans: {
+//         populate: ["product_features"],
+//       },
+//       submitButton: {
+//         populate: true,
+//       },
+//     },
+//   },
+//   seo: {
+//     fields: ["metaTitle", "metaDescription"],
+//     populate: { shareImage: true },
+//   }
+// };
 
 module.exports = (config, { strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
     ctx.query = {
-      populate,
+      populate: 'deep',
       filters: { slug: ctx.query.filters.slug },
       locale: ctx.query.locale,
     };
