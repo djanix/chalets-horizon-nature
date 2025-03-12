a33b86d4cb8680b81e80e9b1178d7a30c228a103
Janic Beauchemin <jbeauchemin33@gmail.com>
2025-03-12 16:17:04 -0400

    Add day picker

--- frontend/src/app/[lang]/components/Availabilities.tsx
+++ frontend/src/app/[lang]/components/Availabilities.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { DayPicker, getDefaultClassNames, DateRange } from "react-day-picker";
+import { useState } from "react";
+
 interface AvailabilitiesProps {
   data: {
     id: string;
@@ -6,13 +11,39 @@ interface AvailabilitiesProps {
 }
 
 export default function Availabilities({ data }: AvailabilitiesProps) {
+  const defaultClassNames = getDefaultClassNames();
+  const [selected, setSelected] = useState<DateRange>();
+
+  console.log(new Date(2025,3,25));
+
   return (
-    <section className="">
-      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+    <section>
+      <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row">
         <div className="flex flex-col justify-center p-6 text-center rounded-lg lg:max-w-md xl:max-w-lg lg:text-left whitespace-pre-wrap">
           {data.description}
         </div>
-        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"></div>
+        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+          <DayPicker
+            mode="range"
+            selected={selected}
+            onSelect={setSelected}
+            min={2}
+            showOutsideDays
+            excludeDisabled
+            disabled={[
+              { before: new Date() },
+              { from: new Date(2025,2,25), to: new Date(2025,2,27) },
+              { from: new Date(2025,2,17), to: new Date(2025,2,19) },
+            ]}
+            footer={
+              (selected?.from && selected?.to) ? `Selected: ${selected.from.toLocaleDateString()} to ${selected.to.toLocaleDateString()}` : "Pick a day."
+            }
+            classNames={{
+              today: `${defaultClassNames.today} font-bold`,
+              footer: 'pt-5 text-center',
+            }}
+          />
+        </div>
       </div>
     </section>
   );
