9fd0cbc2553a4cbade8ce3edf0fbddf69dfcb574
Janic Beauchemin <jbeauchemin33@gmail.com>
2025-03-30 23:53:00 -0400

    Form first pass

--- frontend/src/app/[lang]/components/Reservation.tsx
+++ frontend/src/app/[lang]/components/Reservation.tsx
@@ -14,8 +14,126 @@ export default function Reservation({ data }: ReservationProps) {
     <section>
       <div className="container flex flex-col justify-center mx-auto lg:flex-row lg:justify-between px-6">
         <div className="flex flex-col justify-center pb-6 text-center rounded-lg lg:max-w-md xl:max-w-lg lg:text-left">
-          <h3 className="text-xl font-bold">Réservation à venir.</h3>
-          {/*{data.note}*/}
+          {/*<h3 className="text-xl font-bold">Réservation à venir.</h3>*/}
+
+          <form className="space-y-6">
+            <h3 className="text-xl font-bold">Dates</h3>
+            <div className="flex flex-col lg:flex-row gap-4 items-end">
+              <div className="flex-1">
+                <label htmlFor="startDate" className="block text-sm font-medium text-gray-700">
+                  Arrivée
+                </label>
+                <input id="startDate" type="date" required />
+              </div>
+
+              <div>au</div>
+
+              <div className="flex-1">
+                <label htmlFor="endDate" className="block text-sm font-medium text-gray-700">
+                  Départ
+                </label>
+                <input id="endDate" type="date" required />
+              </div>
+            </div>
+
+            <h3 className="text-xl font-bold">Invités</h3>
+            <div className="flex flex-col lg:flex-row gap-4">
+              <div className="flex-1">
+                <label htmlFor="adults" className="block text-sm font-medium text-gray-700">
+                  Adultes
+                </label>
+                <select id="adults" required>
+                  {[...Array(5).keys()].map((num) => (
+                    <option key={num}>{num}</option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="flex-1">
+                <label htmlFor="kids" className="block text-sm font-medium text-gray-700">
+                  Enfants
+                </label>
+                <select id="kids" required>
+                  {[...Array(5).keys()].map((num) => (
+                    <option key={num}>{num}</option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="flex-1">
+                <label htmlFor="pets" className="block text-sm font-medium text-gray-700">
+                  Animaux
+                </label>
+                <select id="pets" required>
+                  {[...Array(5).keys()].map((num) => (
+                    <option key={num}>{num}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <h3 className="text-xl font-bold">Information</h3>
+            <div className="space-y-4">
+              <div className="flex flex-col lg:flex-row gap-4">
+                <div className="flex-1">
+                  <label htmlFor="firstName" className="block text-sm font-medium text-gray-700">
+                    Prénom
+                  </label>
+                  <input id="firstName" type="text" required />
+                </div>
+                <div className="flex-1">
+                  <label htmlFor="lastName" className="block text-sm font-medium text-gray-700">
+                    Nom
+                  </label>
+                  <input id="lastName" type="text" required />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                  Courriel
+                </label>
+                <input id="email" type="email" required />
+              </div>
+              <div>
+                <label htmlFor="tel" className="block text-sm font-medium text-gray-700">
+                  Téléphone
+                </label>
+                <input id="tel" type="tel" required />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+                Message (Optionnel)
+              </label>
+              <textarea id="message" rows={5} />
+            </div>
+
+            <div className="flex items-start">
+              <div className="flex items-center h-5">
+                <input
+                  id="rules"
+                  type="checkbox"
+                  className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                  required
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="rules" className="font-medium text-gray-700">
+                  J&apos;affirme avoir lu les règlements...
+                </label>
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Soumettre la demande de réservation
+            </button>
+          </form>
+
+          <p className="mt-6">{data.note}</p>
         </div>
 
         <div>
